1545e734e75150cee90876af3cfef5f200a41caa
Mark <markh4b@hotmail.com>
2015-04-02 06:08:32 +0200

    Better bullet trail

--- System.ocg/Mod_BulletTrail.c
+++ System.ocg/Mod_BulletTrail.c
@@ -3,16 +3,48 @@
 /**
  Used for updating the rotation of the trail.
  */
-public func UpdateRot(int angle)
+func UpdateRot(int angle)
 {
 	r = angle;
 }
 
 
-public func Set(int iWidth, int iLength, object pSht)
+func Set(int iWidth, int iLength, object pSht)
 {
 	_inherited(iWidth, iLength, pSht);
 	
 	SetXDir();
 	SetYDir();
+}
+
+
+func DrawTransform()
+{
+	
+	var distance = Distance(x, y, GetX(), GetY());
+	var relative_length = 1000 * distance / ActMap.Travel.Hgt;
+	
+	// skip because nothing has to be transformed
+	// if(!fRemove && l < relative_length) return;
+	
+	// stretch >-<
+	if (fRemove)
+		l = Max(0, l - iSpeed);
+	
+	// stretch <->
+	var h = Min(l, relative_length);
+	
+	var fsin = -Sin(r, 1000), fcos = Cos(r, 1000);
+	
+	var xoff = -(ActMap.Travel.Wdt * w / 1000) / 2;
+	var yoff = 0;
+	
+	var width = +fcos * w / 1000, height = +fcos * h / 1000;
+	var xskew = +fsin * h / 1000, yskew = -fsin * w / 1000;
+	
+	var xadjust = +fcos * xoff + fsin * yoff;
+	var yadjust = -fsin * xoff + fcos * yoff;
+	
+	// set matrix values
+	SetObjDrawTransform(width, xskew, xadjust, yskew, height, yadjust);
 }
