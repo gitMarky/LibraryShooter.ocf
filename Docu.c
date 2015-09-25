da83424ebbb54b33660674bb6f97d1413596d0dc
Mark <markh4b@hotmail.com>
2015-09-25 21:52:11 +0200

    Release v0.1.0

---
+++ Docu.c
@@ -0,0 +1,7 @@
+/** Documentation for this library.
+@title Library: Shooter
+@id index
+@author Marky
+@version 0.1.0
+@credits Zapper (Caedes, which I analyzed and took inspiration from), Hazard Team (for the great Hazard Pack, which is also the base for some of the scripts)
+*/
