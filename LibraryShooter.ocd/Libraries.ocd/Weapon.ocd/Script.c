47cf7b771115dde165d1c8a865b21a6315331ada
Mark <markh4b@hotmail.com>
2017-11-01 23:05:38 +0100

    Weapon: Added selection logic

--- LibraryShooter.ocd/Libraries.ocd/Weapon.ocd/Script.c
+++ LibraryShooter.ocd/Libraries.ocd/Weapon.ocd/Script.c
@@ -1527,3 +1527,69 @@ private func FxIntReloadTimer(object target, proplist effect, int time)
 		effect.percent_old = effect.percent;
 	}
 }
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+//
+// selecting and deselecting the weapon
+
+/**
+ Callback from the inventory system: The weapon was selected.
+ Has it's own callback {@link Library_Weapon#OnSelection}, so
+ that specific behaviour can be overloaded without messing
+ with the rest of the functionality.
+ 
+ @par user The object that is using the weapon.
+ @version 0.3.0
+ */
+public func Selection(object user)
+{
+	is_selected = true;
+	this->OnSelection(user, GetFiremode());
+	return _inherited(user, ...);
+}
+
+
+/**
+ Callback from the inventory system: The weapon was deselected.
+ Has it's own callback {@link Library_Weapon#OnDeselection}, so
+ that specific behaviour can be overloaded without messing
+ with the rest of the functionality.
+ 
+ @par user The object that is using the weapon.
+ @version 0.3.0
+ */
+public func Deselection(object user)
+{
+	is_selected = false;
+	this->OnDeselection(user, GetFiremode());
+	return _inherited(user, ...);
+}
+
+
+
+/**
+ Callback: the weapon was selected. Does nothing by default.
+ 
+ @par user The object that is using the weapon.
+ @par firemode A proplist containing the fire mode information.
+ @version 0.3.0
+ */
+public func OnSelection(object user, proplist firemode)
+{
+	
+}
+
+
+/**
+ Callback: the weapon was selected. Does nothing by default.
+ 
+ @par user The object that is using the weapon.
+ @par firemode A proplist containing the fire mode information.
+ @version 0.3.0
+ */
+public func OnDeselection(object user, proplist firemode)
+{
+	
+}
