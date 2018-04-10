6c07b6a55e9e3d07440a24ff23ecd7c642e5c01e
Mark <markh4b@hotmail.com>
2018-04-10 21:49:54 +0200

    Fix left/right mouse button fire modes, sort of

--- LibraryShooter.ocd/Plugins.ocd/FiremodeByUse.ocd/Script.c
+++ LibraryShooter.ocd/Plugins.ocd/FiremodeByUse.ocd/Script.c
@@ -12,7 +12,9 @@ static const WEAPON_Firemode_Secondary = "secondary";
  */
 public func OnPressUse(object user, int x, int y)
 {
-	this->ScheduleSetFiremode(this->~GetFiremodeByName(WEAPON_Firemode_Primary) ?? 0); // TODO - better than not working at all, but still a temporary solution
+	// May need scheduled fire mode change - currently you can press left and right and get both firemodes at the same time
+	this->SetFiremode(this->~GetFiremodeByName(WEAPON_Firemode_Primary) ?? 0, true); // TODO - better than not working at all, but still a temporary solution
+	_inherited(user, x, y, ...);
 }
 
 /**
@@ -24,15 +26,47 @@ public func OnPressUse(object user, int x, int y)
  */
 public func OnPressUseAlt(object user, int x, int y)
 {
-	this->ScheduleSetFiremode(this->~GetFiremodeByName(WEAPON_Firemode_Secondary) ?? 1); // TODO - better than not working at all, but still a temporary solution
+	this->SetFiremode(this->~GetFiremodeByName(WEAPON_Firemode_Secondary) ?? 1, true); // TODO - better than not working at all, but still a temporary solution
+
+	user->StartAim(this);
+	this->ControlUseHolding(user, x, y);
+	
+	_inherited(...);
 }
 
+/**
+ Callback: The alternative use button is held. Called in regular intervals by {@link Library_Firearm#ControlUseAltHolding}.@br
+ @par user The object that is using the weapon.
+ @par x The x coordinate the user is aiming at. Relative to the user.
+ @par y The y coordinate the user is aimint at. Relative to the user.
+ */
+public func OnHoldingUseAlt(object user, int x, int y)
+{
+	this->ControlUseHolding(user, x, y);
+}
 
 /**
- Callback from {@link Library_Weapon#ControlUserStop}, 
- so that you do not have to overload the entire function.
+ Callback: Released the alternative use button. Called by {@link Library_Firearm#ControlUseAltStop}.@br
+ @par user The object that is using the weapon.
+ @par x The x coordinate the user is aiming at. Relative to the user.
+ @par y The y coordinate the user is aimint at. Relative to the user.
  */
-public func OnUseStop(object user, int x, int y)
+public func OnUseAltStop(object user, int x, int y)
 {
-	this->ResetScheduledFiremode();
+	this->ControlUseStop(user, x, y);
+}
+
+/**
+ This is executed when the user cancels the alternative use (usually by pressing a dedicated cancel button; must be defined, not standard in OpenClonk).@br@br
+
+ The function does the following:@br
+ - call {@link Library_Firearm#OnCancelUseAlt}@br
+ @par user The object that is using the weapon.
+ @par x The x coordinate the user is aiming at. Relative to the user.
+ @par y The y coordinate the user is aimint at. Relative to the user.
+ @version 0.1.0
+ */
+public func ControlUseAltCancel(object user, int x, int y)
+{
+	this->ControlUseCancel(user, x, y);
 }
