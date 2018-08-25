e07a6fb81b233020d281d6f1559ebf05f4e7a73f
Mark <markh4b@hotmail.com>
2018-08-25 17:04:08 +0200

    Reload: States progress
    SOme progress, but this is still confused. On the one hand, the weapon
    has to track its reloading state. On the other hand, the reload effect
    has to track it (at least a little), but the most efficient way would be
    to have the same effect for all stages instead of having one effect per
    stage.

--- LibraryShooter.ocd/Plugins.ocd/ReloadLogicStates.ocd/Script.c
+++ LibraryShooter.ocd/Plugins.ocd/ReloadLogicStates.ocd/Script.c
@@ -5,6 +5,10 @@
 	You have to call the inherited function in:
 	- Construction()
 	if you use this.
+	
+	@note
+	Issues: Handle multiple fire modes how?
+	Theoretically there can be fire modes that use the same ammo, so it makes sense to share the reload state between ammo types.
 
 	@author Marky
  */
@@ -18,7 +22,7 @@ local firearm_reload;
 func Construction(object by)
 {
 	firearm_reload = firearm_reload ?? {};
-	firearm_reload.current_state = nil; // Ready to go!
+	firearm_reload.current_state = nil; // Ready to go. Can be changed to array or proplist in case that multiple states are necessary.
 	return _inherited(by, ...);
 }
 
@@ -36,6 +40,38 @@ func IsReloading()
 
 
 /**
+	Gets the current reload state.
+ */
+func GetReloadState()
+{
+	return firearm_reload.current_state ?? GetReloadStateDefault();
+}
+
+
+/**
+	Gets the default reload state that the weapon starts reloading from.
+ */
+func GetReloadStateDefault()
+{
+	return nil;
+}
+
+
+/**
+	Sets the current reload state.
+	
+	@par state the reload state. A value of {@code nil} means, that
+	     the weapon is reloaded.
+ */
+func SetReloadState(proplist state)
+{
+	firearm_reload.current_state = state;
+}
+
+
+/* --- Callbacks from the firearm --- */
+
+/**
 	Callback: The weapon reload effect should be created.
 
 	@par user The object that is using the weapon.
@@ -45,8 +81,37 @@ func IsReloading()
  */
 func StartReloadProcess(object user, int x, int y, proplist firemode)
 {
-	CreateEffect(IntReloadStagesEffect, 1, 1, user, x, y, firemode);
+	var state = GetReloadState();
+	if (state)
+	{
+		var process = CreateEffect(IntReloadStagesEffect, 1, 1, user, x, y, firemode);
+		process->SetState(state);
+	}
+	else
+	{
+		FatalError("Cannot start reloading: No default state is set");
+	}
 }
+
+
+/**
+	Callback: the weapon user cancelled reloading. Does nothing by default.@br
+
+	@par user The object that is using the weapon.
+	@par x The x coordinate the user is aiming at. Relative to the user.
+	@par y The y coordinate the user is aimint at. Relative to the user.
+	@par firemode A proplist containing the fire mode information.
+	@par requested_by_user Is {@c true} if the user releases the use button while the weapon is reloading. Otherwise, for example if the user changes the firemode is {@c false}.
+ */
+public func OnCancelReload(object user, int x, int y, proplist firemode, bool requested_by_user)
+{
+	var state = GetReloadState();
+	if (state)
+	{
+		SetReloadState(state->GetCancelState());
+	}
+}
+
 
 /* --- Internal --- */
 
@@ -64,12 +129,22 @@ local IntReloadStagesEffect = new Effect
 		this.progress = 0;
 		this.is_reloaded = false;
 	},
+	
+	SetState = func (proplist state)
+	{
+		this.state = state;
+		state->~OnStart(this.user, this.x, this.y, this.firemode);
+	},
 
 	Timer = func (int time)
 	{
 		// Cancel if user cannot reload
-		if (!this.Target->IsUserReadyToReload())
+		if (!this.Target->IsUserReadyToReload() || !this.state)
 		{
+			if (this.state)
+			{
+				this.state->~OnCancel(this.user, this.x, this.y, this.firemode);
+			}
 			this.Target->CancelReload(this.user, this.x, this.y, this.firemode, false);
 		}
 	
@@ -79,10 +154,16 @@ local IntReloadStagesEffect = new Effect
 		this.progress = this.percentage - this.percent_old;
 
 		// Check if the reloading process is finished based on the reloading delay of the firemode
-		if (time > this.firemode->GetReloadDelay() && !this.is_reloaded)
+		if (time > this.state->GetDelay() && !this.is_reloaded)
 		{
 			this.is_reloaded = true;
-
+		}
+		
+		// Done reloading?
+		if (this.is_reloaded)
+		{
+			// TODO
+			
 			// Do the reload if anything is necessary and end the effect if successful
 			if (this.Target->DoReload(this.user, this.x, this.y, this.firemode))
 			{
@@ -120,34 +201,72 @@ local IntReloadStagesEffect = new Effect
 static const Firearm_ReloadState = new Global
 {
 	is_defined = false,
-	state_next = nil,
+	state_next = nil,   // This state is assumed if the current state was finished successfully.
+	state_cancel = nil, // This state is assumed if the current state was cancelled.
+	delay = 1,          // The state takes this long.
 
 	// Use this function as a constructor for the state; Call
 	Create = func ()
 	{
+		// Repeat the state upon cancellation, as a default
+		SetCancelState(this);
+		// Allow user-defined setup
 		this->~Setup();
+		// Finish
 		is_defined = true;
 		return this;
 	},
 
 	// --- Getters
+	
+	// The state lasts this long
+	GetDelay = func ()
+	{
+		return this.delay;
+	},
 
-	// Gets the state that should be set after this one.
+	// Gets the state that should be set after this one,
+	// if the action was successful.
+	//
 	// If the function returns 'nil', this means that reloading
-	// is done and 
+	// is done.
 	GetNextState = func ()
 	{
 		return this.state_next;
-	}
+	},
+	
+	// Gets the state that should be set after this one,
+	// if the action was cancelled.
+	//
+	// If the function returns 'nil', this means that reloading
+	// is done.
+	GetCancelState = func ()
+	{
+		return this.state_cancel;
+	},
 	
 	// --- Setters
+	
+	SetDelay = func (int delay)
+	{
+		AssertOnlyBeforeCreation();
+		this.delay = delay;
+		return this;
+	},
 	
 	SetNextState = func (proplist state)
 	{
 		AssertOnlyBeforeCreation();
 		this.state_next = state;
 		return this;
-	}
+	},
+	
+	SetCancelState = func (proplist state)
+	{
+		AssertOnlyBeforeCreation();
+		this.state_cancel = state;
+		return this;
+	},
 	
 	// --- Internals
 	
@@ -157,5 +276,5 @@ static const Firearm_ReloadState = new Global
 		{
 			FatalError("You should called this function after the state was created with ->Create(). This is prohibited.");
 		}
-	}
+	},
 };
