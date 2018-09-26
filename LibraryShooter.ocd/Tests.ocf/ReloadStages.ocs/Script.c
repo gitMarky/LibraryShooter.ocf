7bef8fa5e286db22ed606cfc66c8c39147f26d16
Mark <markh4b@hotmail.com>
2018-09-26 22:01:53 +0200

    Reload: Test refactoring 2

--- LibraryShooter.ocd/Tests.ocf/ReloadStages.ocs/Script.c
+++ LibraryShooter.ocd/Tests.ocf/ReloadStages.ocs/Script.c
@@ -14,11 +14,12 @@ func InitializePlayer(int plr)
 
 /* --- The actual tests --- */
 
-global func Test_Init(id weapon_type)
+global func Test_Init(string message, id weapon_type)
 {
 	var test = CurrentTest();
 	if (!test.initialized)
 	{
+		Log(message);
 		test.user = GetHiRank(test.player);
 		test.user->SetPosition(LandscapeWidth() / 2, test.user->GetY());		
 		test.weapon = test.user->CreateContents(weapon_type);
@@ -26,12 +27,34 @@ global func Test_Init(id weapon_type)
 		test.loading = false;
 		test.states = [];
 	}
+	return true;
 }
 
 global func Test_Reset()
 {
 	CurrentTest().initialized = false;
 }
+
+global func Test_ReloadSequence(int user_initial_ammo, int weapon_initial_ammo, int user_final_ammo, int weapon_final_ammo, array expected)
+{
+	if (CurrentTest().loading)
+	{
+		doTestTransitions(expected); // Check the transitions
+		doTestAmmo("Final", user_final_ammo, weapon_final_ammo); // Check the remaining ammo
+		Test_Reset();
+		return Evaluate();
+	}
+	else
+	{
+		SetupAmmo(user_initial_ammo, weapon_initial_ammo);
+
+		// Do it!
+		CurrentTest().weapon->StartReload(CurrentTest().user, 100, 0, true);
+		CurrentTest().loading = true;
+		return Wait(20);
+	}
+}
+
 
 global func SetupAmmo(int user_initial_ammo, int weapon_initial_ammo)
 {
@@ -42,13 +65,13 @@ global func SetupAmmo(int user_initial_ammo, int weapon_initial_ammo)
 
 		// Sanity check
 		doTest("Initial user ammo was %d, should be %d", CurrentTest().user->GetAmmo(Dummy), user_initial_ammo);
-		doTest("Initial weapon ammo was %d, should be %d", CurrentTest().weapon->GetAmmo(Dummy), weapon_initial_ammo);}
+		doTest("Initial weapon ammo was %d, should be %d", CurrentTest().weapon->GetAmmo(Dummy), weapon_initial_ammo);
 }
 
 global func doTestAmmo(string description, int user_ammo, int weapon_ammo)
 {
-		doTest(Format("%s user ammo was \%d, should be \%d", description), CurrentTest().user->GetAmmo(Dummy), user_ammo);
-		doTest(Format("%s weapon ammo was \%d, should be \%d", description), CurrentTest().weapon->GetAmmo(Dummy), weapon_ammo);
+		doTest(Format("%s user ammo was %%d, should be %%d", description), CurrentTest().user->GetAmmo(Dummy), user_ammo);
+		doTest(Format("%s weapon ammo was %%d, should be %%d", description), CurrentTest().weapon->GetAmmo(Dummy), weapon_ammo);
 }
 
 global func doTestTransitions(array expected)
@@ -67,73 +90,34 @@ global func doTestTransitions(array expected)
 
 global func Test1_OnStart()
 {
-	Log("Test for reloading: Reloading with container plugin, default settings");
-	return true;
+	return Test_Init("Test for reloading: Reloading with container plugin, default settings", Realod_Container_Default);
 }
-
 global func Test1_Execute()
 {
-	Test_Init(Realod_Container_Default);
-	
-	var expected = ["Reload_Container_Prepare",
-	                "Reload_Container_EjectAmmo",
-	                "Reload_Container_InsertAmmo",
-	                "Reload_Container_Close",
-	                "Reload_Container_ReadyWeapon",
-	                nil];
-
-	if (CurrentTest().loading)
-	{
-		doTestTransitions(expected); // Check the transitions
-		doTestAmmo("Final", 4, 10); // Check the remaining ammo
-		Test_Reset();
-		return Evaluate();
-	}
-	else
-	{
-		SetupAmmo(14, 0);
-
-		// Do it!
-		CurrentTest().weapon->StartReload(CurrentTest().user, 100, 0, true);
-		CurrentTest().loading = true;
-		return Wait(20);
-	}
+	return Test_ReloadSequence(14,  0, 
+	                            4, 10,
+	                           ["Reload_Container_Prepare",
+	                            "Reload_Container_EjectAmmo",
+	                            "Reload_Container_InsertAmmo",
+	                            "Reload_Container_Close",
+	                            "Reload_Container_ReadyWeapon",
+	                            nil]);
 }
-
 
 global func Test2_OnStart()
 {
-	Log("Test for reloading: Reloading with container plugin, default settings");
-	return true;
+	return Test_Init("Test for reloading: Reloading with container plugin, default settings", Realod_Container_Default);
 }
-
 global func Test2_Execute()
 {
-	Test_Init(Realod_Container_Default);
-	
-	var expected = ["Reload_Container_Prepare",
-	                "Reload_Container_EjectAmmo",
-	                "Reload_Container_StashStart}",
-	                "Reload_Container_StashFinish",
-	                "Reload_Container_InsertAmmo",
-	                "Reload_Container_Close",
-	                "Reload_Container_ReadyWeapon",
-	                nil];
-	                
-	if (CurrentTest().loading)
-	{
-		doTestTransitions(expected); // Check the transitions
-		doTestAmmo("Final", 9, 10);  // Check the remaining ammo
-		Test_Reset();
-		return Evaluate();
-	}
-	else
-	{
-		SetupAmmo(14, 5);
-
-		// Do it!
-		CurrentTest().weapon->StartReload(CurrentTest().user, 100, 0, true);
-		CurrentTest().loading = true;
-		return Wait(20);
-	}
+	return Test_ReloadSequence(14,  5, 
+	                            9, 10,
+	                           ["Reload_Container_Prepare",
+	                            "Reload_Container_EjectAmmo",
+	                            "Reload_Container_StashStart}",
+	                            "Reload_Container_StashFinish",
+	                            "Reload_Container_InsertAmmo",
+	                            "Reload_Container_Close",
+	                            "Reload_Container_ReadyWeapon",
+	                            nil]);
 }
