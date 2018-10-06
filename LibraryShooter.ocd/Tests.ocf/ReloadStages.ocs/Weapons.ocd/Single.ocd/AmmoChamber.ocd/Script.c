7fedb962f3de30ae939e49e037db0e875acdf75d
Mark <markh4b@hotmail.com>
2018-10-06 15:59:01 +0200

    Reload: Test for single ammo with ammo chamber

---
+++ LibraryShooter.ocd/Tests.ocf/ReloadStages.ocs/Weapons.ocd/Single.ocd/AmmoChamber.ocd/Script.c
@@ -0,0 +1,25 @@
+#include Library_Firearm
+#include Weapon_Base
+#include Library_AmmoManager
+#include Plugin_Firearm_AmmoLogic
+#include Plugin_Firearm_ReloadFromAmmoSource
+#include Plugin_Firearm_ReloadStates
+#include Plugin_Firearm_ReloadStates_Single
+#include Plugin_Firearm_AmmoChamber
+
+local Description = "Reloading with single ammo plugin, default settings";
+
+public func GetAmmoSource(id ammo)
+{
+	return AMMO_Source_Local;
+}
+
+public func GetAmmoReloadContainer()
+{
+	return Contained();
+}
+
+func PlaySoundInsertShell()
+{
+	PushBack(CurrentTest().states, "Single_Ammo_Insert_Counter"); // Special non-existing state
+}
