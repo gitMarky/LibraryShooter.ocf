f68299f1377c181ae2457cb31c74cb6f589077a2
Mark <markh4b@hotmail.com>
2019-05-05 13:21:33 +0200

    Added StanceManager with basic test

---
+++ LibraryShooter.ocd/Libraries.ocd/StanceManager.ocd/Script.c
@@ -0,0 +1,149 @@
+/**
+	Stance manager.
+	
+	You can save known stances in a local property StanceMap.
+	Originally, the library was designed without a StanceMap,
+	but it makes handling the stances easier.
+
+	@author Marky
+ */
+
+/* --- Properties --- */
+
+local lib_stance_manager;
+
+/* --- Engine Callbacks --- */
+
+func Construction(object by)
+{
+	lib_stance_manager = lib_stance_manager ?? {};
+	lib_stance_manager.current_stance = lib_stance_manager.current_stance ?? {};
+	return _inherited(by, ...);
+}
+
+
+/* --- Interface --- */
+
+public func GetStance(any channel)
+{
+	return lib_stance_manager.current_stance[GetStanceChannel(channel)];
+}
+
+public func SetStance(any stance, any channel, bool force)
+{
+	AssertNotNil(stance);
+	stance = GetStanceDefinition(stance);
+	var current = GetStance(channel);
+	if (current == nil || force || current->HasTransitionTo(stance))
+	{
+		lib_stance_manager.current_stance[GetStanceChannel(channel)] = stance;
+		return true;
+	}
+	return false;
+}
+
+/* --- Internals --- */
+
+func GetStanceChannel(any channel)
+{
+	var default = "0";
+	var type = GetType(channel);
+	if (type == C4V_Nil || (type == C4V_String && GetLength(channel) == nil))
+	{
+		return default;
+	}
+	else
+	{
+		return Format("%v", channel);
+	}
+}
+
+
+/**
+	Gets the stance definition.
+	
+	@par stance In case this is a proplist it will be returned directly.
+	            Passing a string will prefer stances in {@code this.StanceMap}
+	            where {@code stance.Name} equals the string, otherwise of the
+	            property name is equal to the string.
+	@note For {@code StanceMap = { StanceAbc = {Name = "xyz"}, xyz = {Name = "StanceAbc"}}}
+	      {@code GetStanceDefinition("xyz")} will return the first stance in the list, because the name is preferred.
+	      {@code GetStanceDefinition("StanceAbc")} will also return the first stance in the list, because they are
+	      searched in order, but while the name property "xyz" does not match the name of the property matches.
+ */
+func GetStanceDefinition(any stance)
+{
+	if (GetType(stance) == C4V_PropList)
+	{
+		return stance;
+	}
+	if (GetType(stance) == C4V_String)
+	{
+		if (this.StanceMap)
+		{
+			for (var name in GetProperties(this.StanceMap))
+			{
+				var found = GetProperty(name, this.StanceMap);
+				if (found && found.Name == stance)
+				{
+					return found;
+				}
+				else if (name == stance)
+				{
+					return found;
+				}
+			}
+			return nil;
+		}
+		else
+		{
+			FatalError("StanceMap not defined");
+		}
+	}
+	else
+	{
+		FatalError("Unknown stance \"%s\"", stance);
+	}
+}
+
+/* --- Data Structure --- */
+
+static const StanceDefinition = new Global
+{
+	Transitions = nil, // Array, defines stances that you can change to from this stance
+
+	// --- Interface
+	
+	/**
+		Adds a valid transition from this stance
+		to another, unidirectional.
+		
+		@par to_stance The valid target stance.
+		@return proplist Returns {@code to_stance}, so that you can 
+		                 define further transitions from there on.
+		                 Looks more natural when scripting.
+	 */
+	AddTransition = func (proplist to_stance)
+	{
+		if (this.Transitions == nil)
+		{
+			this.Transitions = [];
+		}
+		if (!HasTransitionTo(this.Transitions, to_stance))
+		{
+			PushBack(this.Transitions, to_stance);
+		}
+		return to_stance;
+	},
+	
+	/**
+		Finds out whether you can transition to the desired stance.
+		
+		@par to_stance The target stance.
+		@return {@code true} if the transition is valid.
+	 */
+	HasTransitionTo = func (proplist to_stance)
+	{
+		return IsValueInArray(this.Transitions, to_stance);
+	},
+};
---
+++ LibraryShooter.ocd/Tests.ocf/Stance.ocs/Script.c
@@ -0,0 +1,123 @@
+
+static const POSE_STANDING = "Standing";
+static const POSE_CROUCHING = "Crouching";
+static const POSE_PRONE = "Prone";
+
+static const WEAPON_IDLE = "WeaponIdle";
+static const WEAPON_READY = "WeaponReady";
+static const WEAPON_AIMING = "WeaponAiming";
+
+func InitializePlayer(int plr)
+{
+	// Set zoom to full map size.
+	SetPlayerZoomByViewRange(plr, 300, nil, PLRZOOM_Direct);
+
+	// No FoW to see everything happening.
+	SetFoW(false, plr);
+
+	// Start!
+	LaunchTest(1);
+	return;
+}
+
+global func CreateStanceManager()
+{
+	// Standing state
+	var standing = new StanceDefinition { Name = POSE_STANDING, };
+	var crouching = new StanceDefinition { Name = POSE_CROUCHING, };
+	var prone = new StanceDefinition { Name = POSE_PRONE, };
+	
+	// Define transitions
+	standing->AddTransition(crouching)
+	        ->AddTransition(prone)
+	        ->AddTransition(crouching)
+	        ->AddTransition(standing);
+	        
+	// Aiming state
+	var weapon_idle = new StanceDefinition { Name = WEAPON_IDLE, };
+	var weapon_ready = new StanceDefinition { Name = WEAPON_READY, };
+	var weapon_aiming = new StanceDefinition { Name = WEAPON_AIMING, };
+	
+	weapon_idle->AddTransition(weapon_ready)->AddTransition(weapon_idle);
+	weapon_ready->AddTransition(weapon_aiming)->AddTransition(weapon_ready);
+
+	// Add everything to the stance maanger
+
+	var manager = CreateObject(Library_StanceManager);
+
+	manager->SetStance(standing);
+	manager->SetStance(weapon_idle, 1);
+	
+	// So that it can be retrieved via string
+
+	manager.StanceMap = { "pose0" = standing, "pose1" = crouching, "pose2" = prone,
+	                      "weap0" = weapon_idle, "weap1" = weapon_ready, "weap2" = weapon_aiming};
+	return manager;
+}
+
+global func doTestTransition(object manager, any channel, string from, string to, bool result)
+{
+	var desc = Format("[%s => %s](%v): ", from, to, channel);
+
+	var final;
+	if (result)
+	{
+		final = to;
+	}
+	else
+	{
+		final = from;
+	}
+
+	doTest(Format("%s%s", desc, "Initial stance should be \"%s\", got \"%s\""), from, manager->GetStance().Name);
+	doTest(Format("%s%s", desc, "Transition should return %v, got %v"), result, manager->SetStance(to));
+	doTest(Format("%s%s", desc, "Final stance should be \"%s\", got \"%s\""), final, manager->GetStance().Name);
+}
+
+/* --- The actual tests --- */
+
+
+// --------------------------------------------------------------------------------------------------------
+
+global func Test1_OnStart() { return true; }
+global func Test1_Execute()
+{
+	var manager = CreateStanceManager();
+	
+	Log("Stance manager tracks stances correctly in GetStance()");
+
+	doTest("Stance for channel(nil) should be \"%s\", got \"%s\"", POSE_STANDING, manager->GetStance().Name);
+	doTest("Stance for channel(0) should be \"%s\", got \"%s\"", POSE_STANDING, manager->GetStance(0).Name);
+	doTest("Stance for channel(1) should be \"%s\", got \"%s\"", WEAPON_IDLE, manager->GetStance(1).Name);
+
+	return Evaluate();
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+global func Test2_OnStart() { return true; }
+global func Test2_Execute()
+{
+	var manager = CreateStanceManager();
+	
+	Log("SetStance() works correctly for the known transitions");
+
+	// Standing
+	doTestTransition(manager, 0, POSE_STANDING, POSE_STANDING, false);
+	doTestTransition(manager, 0, POSE_STANDING, POSE_PRONE, false);
+	doTestTransition(manager, 0, POSE_STANDING, POSE_CROUCHING, true);
+	
+	// Crouching
+	doTestTransition(manager, 0, POSE_CROUCHING, POSE_CROUCHING, false);
+	doTestTransition(manager, 0, POSE_CROUCHING, POSE_PRONE, true);
+	
+	// Prone
+	doTestTransition(manager, 0, POSE_PRONE, POSE_PRONE, false);
+	doTestTransition(manager, 0, POSE_PRONE, POSE_STANDING, false);
+	
+	// Back
+	doTestTransition(manager, 0, POSE_PRONE, POSE_CROUCHING, true);
+	doTestTransition(manager, 0, POSE_CROUCHING, POSE_STANDING, true);
+
+	return Evaluate();
+}
