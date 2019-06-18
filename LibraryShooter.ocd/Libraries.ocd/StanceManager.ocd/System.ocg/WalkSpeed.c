f4910b16dfa4a864f24dcdf75d3b1a40d3bf2224
Mark <markh4b@hotmail.com>
2019-06-18 20:52:19 +0200

    Split the behaviours into individual files.

---
+++ LibraryShooter.ocd/Libraries.ocd/StanceManager.ocd/System.ocg/WalkSpeed.c
@@ -0,0 +1,45 @@
+/**
+ * Applies walk speed, as in the OpenClonk AimManager.
+ */
+static const StanceBehaviour_WalkSpeed = new Global
+{
+	/**
+	 * Constructor function.
+	 */
+	Create = func (int walk_speed, int walk_back)
+	{
+		var behaviour = new StanceBehaviour_WalkSpeed { };
+		// Define properties only if they were provided
+		if (walk_speed != nil)
+		{
+			behaviour.WalkSpeed = walk_speed;
+		}
+		if (walk_back != nil)
+		{
+			behaviour.WalkBack = walk_back;
+		}
+		return behaviour;
+	},
+
+	OnStanceSet = func (object clonk, any channel, bool force)
+	{
+		var walk_speed = this["WalkSpeed"];
+		var walk_back = this["WalkBack"];
+
+		if (walk_speed != nil)
+		{
+			clonk->~PushActionSpeed("Walk", walk_speed);
+		}
+
+		if (walk_back != nil)
+		{
+			clonk->~SetBackwardsSpeed(walk_back);
+		}
+	},
+
+	OnStanceReset = func (object clonk, any channel, bool force)
+	{
+		clonk->~PopActionSpeed("Walk");
+		clonk->~SetBackwardsSpeed(nil);
+	},
+};
